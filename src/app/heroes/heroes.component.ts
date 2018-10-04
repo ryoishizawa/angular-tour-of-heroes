4af8eb63a37b8c8acbc01a2389e7195efd89344e
ryoishizawa <ryoishizawa298142@gmail.com>
2018-10-04 19:00:47 +0800

    Tutorial from part 3 to part 4 'Add a click event binding'

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -6,8 +8,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  hero = 'Windstorm';
-  
+  hero: Hero = {
+    id: 1,
+    name: 'windstorm'
+  };
+  heroes = HEROES;
+
   constructor() { }
 
   ngOnInit() {
