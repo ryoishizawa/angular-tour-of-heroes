36a7a747fed56b87f423b9e7e5dbf4f38569e31e
ryoishizawa <ryoishizawa298142@gmail.com>
2018-10-08 16:21:24 +0900

    Revert "Completed Tutorial Part 4"
    This reverts commit 4b3c4849b4c1f55923ceb5341663119265026675.

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -7,18 +7,16 @@ import { HEROES } from '../mock-heroes';
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
-
 export class HeroesComponent implements OnInit {
-
+  hero: Hero = {
+    id: 1,
+    name: 'windstorm'
+  };
   heroes = HEROES;
-  selectedHero: Hero;
 
   constructor() { }
 
   ngOnInit() {
   }
 
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-  }
 }
